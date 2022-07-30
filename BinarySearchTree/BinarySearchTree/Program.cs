e8afaf57c3ba5597b2314a920a7955ec0899bb81
ravi04ez <ezekie.em@gmail.com>
2022-07-30 14:49:24 +0530

    Added BST

--- BinarySearchTree/BinarySearchTree/Program.cs
+++ BinarySearchTree/BinarySearchTree/Program.cs
@@ -9,7 +9,32 @@ namespace BinarySearchTree
             Console.WriteLine("\t\t\t\t##########################################################");
             Console.WriteLine("\t\t\t\t\tWELCOME TO BINARY SEARCH TREE PROGRAM ");
             Console.WriteLine("\t\t\t\t##########################################################");
-            Console.ReadKey();
+            int choice;
+            while (true)
+            {
+
+
+                Console.WriteLine("\n0. Exit");
+                Console.WriteLine("1.Create Binary Search Tree byadding 56 and then adding 30 & 70");
+                Console.Write("\nEnter your choice : ");
+                choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        BinarySearchTree<int> binary1 = new BinarySearchTree<int>(56);
+                        binary1.Insert(30);
+                        binary1.Insert(70);
+                        Console.WriteLine("\nBinary Search Tree (in inorder traversal) is :-");
+                        binary1.Display();
+                        break;
+                    case 0:
+                        return;
+                    default:
+                        Console.WriteLine("Please choose correct Question Number");
+                        break;
+                }
+                Console.ReadKey();
+            }
         }
     }
 }
