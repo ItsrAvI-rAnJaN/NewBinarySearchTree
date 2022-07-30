95a16f6691d59d5d02fef1dab82e2c78bc20d898
ravi04ez <ezekie.em@gmail.com>
2022-07-30 14:41:16 +0530

    Wlcm Msg for BST

---
+++ BinarySearchTree/BinarySearchTree/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BinarySearchTree
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t\t\t##########################################################");
+            Console.WriteLine("\t\t\t\t\tWELCOME TO BINARY SEARCH TREE PROGRAM ");
+            Console.WriteLine("\t\t\t\t##########################################################");
+            Console.ReadKey();
+        }
+    }
+}
